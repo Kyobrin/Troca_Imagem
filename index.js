809eda917febe16f1fd22c5c9109637f23a93e60
Leozllin <104958629+Kyobrin@users.noreply.github.com>
2023-05-04 21:17:36 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+img = document.getElementById('imagem')
+function clicou(){
+    img.setAttribute ('src', 'https://w7.pngwing.com/pngs/198/750/png-transparent-bill-cipher-dipper-pines-mabel-pines-cipher-disk-bill-miscellaneous-color-bill-cipher-thumbnail.png')
+}
